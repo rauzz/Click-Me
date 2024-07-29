8f9f0a044e445068f8293cf2aaad12a77169bc93
Marcin Izdebski <marcinok24@gmail.com>
2024-07-29 16:24:17 +0200

    fix

---
+++ dist/strings.js
@@ -0,0 +1,16 @@
+export const phrases = [
+    "come on",
+    "almost there",
+    "close one",
+    "keep trying",
+    "faster",
+    "so slow",
+    "weeeee",
+    "i'm zoomin",
+    "keep it up",
+    "untouchable",
+    "literally ninja",
+    "click me",
+    "hahaha",
+    "phew",
+];
