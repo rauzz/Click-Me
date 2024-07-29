05f12b9336210d3caf97e30ff54f572ea1691630
Marcin Izdebski <marcinok24@gmail.com>
2024-07-29 12:49:27 +0200

    randomize button text on hover

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,15 @@
+import { phrases } from "./strings.js";
 const btn = document.querySelector(".button");
 const moveBtn = function () {
-    let posX;
-    let posY;
     function randomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
     const randomX = randomInt(0, 650);
     const randomY = randomInt(0, 740);
-    posX = randomX;
-    posY = randomY;
+    const posX = randomX;
+    const posY = randomY;
     btn.style.left = posX + "px";
     btn.style.top = posY + "px";
+    btn.innerText = phrases[randomInt(0, 13)];
 };
+btn.addEventListener("mouseover", moveBtn);
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
+import { phrases } from "./strings.js";
+
 const btn: HTMLElement = document.querySelector(".button");
 
 const moveBtn = function () {
-	let posX: number;
-	let posY: number;
-
+   
 	function randomInt(min: number, max: number): number {
-		return Math.floor(Math.random() * (max - min + 1) + min);
+      return Math.floor(Math.random() * (max - min + 1) + min);
 	}
-
+   
 	const randomX = randomInt(0, 650);
 	const randomY = randomInt(0, 740);
 
-	posX = randomX;
-	posY = randomY;
+   const posX: number= randomX;
+   const posY: number= randomY;
 
 	btn.style.left = posX + "px";
 	btn.style.top = posY + "px";
+
+   btn.innerText = phrases[randomInt(0, 13)];
 };
+
+btn.addEventListener("mouseover", moveBtn);
